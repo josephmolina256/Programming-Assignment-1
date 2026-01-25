273c86b2c24c88803dbce2749b6e815030cf6bab
Josh Caron <joshua@minshane.com>
2026-01-25 18:03:35 -0500

    GS Algorithm Intialization

--- src/matcher.py
+++ src/matcher.py
@@ -1,3 +1,43 @@
-def gale_shapley(hospital_prefs, student_prefs):
-    ...
 
+
+# Need a parsing function to turn data into hospital, student preferences
+
+
+def gale_shapley(n, hospital_preferences, student_preferences):
+
+    free_hospitals = list(range(n))
+    h_match = [None]*n
+    s_match = [None]*n
+    next_i = [0]*n
+
+    while free_hospitals:
+        h = free_hospitals.pop(0)
+        s = hospital_preferences[h][next[h]]
+        next_i[h] += 1
+
+        # if the student is unmatcged it will (perhaps, temporarily) accept
+        if s_match[s] is None:
+            s_match[s]=h
+            h_match[h]=s
+
+        # otherwise we'll compare the current hospital to the one already assigned
+        else:
+            flag = False
+            assigned_h = s_match[s]
+            for hosp in student_preferences[s]:
+                if hosp == h: 
+                    flag = True #student preferes current to assigned
+                    break
+                if hosp == assigned_h: #student preferes assigned to current
+                    break
+
+            # reassign hospital if necessary, turn old hospital free
+            if flag:
+                s_match[s]=h
+                h_match[h]=s
+                h_match[assigned_h]=None
+                free_hospitals.append(assigned_h)      
+            else:
+                free_hospitals.append(h) 
+
+    return h_match
